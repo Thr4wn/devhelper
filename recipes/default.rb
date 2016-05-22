49b2029bd1fa12bee097c81307dd1aafa4b2ba45
Alexander Bird <alexander.bird.42@gmail.com>
2016-05-22 21:50:14 +0000

    added jenkins_plugin directives

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,21 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
 include_recipe 'jenkins::master'
 
 normal['jenkins']['master']['jvm_options'] = '-Djenkins.install.runSetupWizard=false'
+
+jenkins_plugin 'cloudbees-folder'
+jenkins_plugin 'build-timeout'
+jenkins_plugin 'credentials-binding'
+jenkins_plugin 'ws-cleanup'
+jenkins_plugin 'gradle'
+jenkins_plugin 'github-organization-folder'
+jenkins_plugin 'github-branch-source'
+jenkins_plugin 'workflow-aggregator'
+jenkins_plugin 'build-pipeline-plugin'
+jenkins_plugin 'pipeline-stage-view'
+jenkins_plugin 'git'
+jenkins_plugin 'matrix-auth'
+
